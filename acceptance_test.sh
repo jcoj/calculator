cf1989ed9d4a9077ec9f00f3b2ca5a0ba20b179b
Joao Cosme <joao.cosme@bonial.com>
2018-07-30 12:17:29 +0200

    changed curl run

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,3 @@
-#!/bin/bash -
-test $(curl "http://172.16.60.68:8765/sum=?a=1\&b=2") -eq 3
+#!/bin/bash 
+test $(curl "http://172.16.60.68:8765/sum?a=2&b=1") -eq 3
+#test $(curl 172.16.60.68:8765/sum=?a=1\&b=2) -eq 3
